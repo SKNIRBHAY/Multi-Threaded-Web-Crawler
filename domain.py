84cda773d674cc3a493c6c7af253ea94798fac11
SKNIRBHAY <sknirbhay@gmail.com>
2017-06-05 21:48:01 -0700

    Add files via upload

---
+++ domain.py
@@ -0,0 +1,17 @@
+from urllib.parse import urlparse
+
+# Get domain name (example.com)
+def get_domain_name(url):
+	try:
+		results = get_sub_domain_names(url).split('.');
+		return results[-2] + '.' + results[-1];
+	except:
+		return ''
+
+def get_sub_domain_names(url):
+	try:
+		return urlparse(url).netloc; # Returns only the network location of a website
+	except:
+		return '';
+
+		
