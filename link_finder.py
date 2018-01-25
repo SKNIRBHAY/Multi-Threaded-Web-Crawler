dbed584ecdfbe575052016a06cae3fdde16e6856
SKNIRBHAY <sknirbhay@gmail.com>
2018-01-24 21:04:43 -0800

    Update link_finder.py

--- link_finder.py
+++ link_finder.py
@@ -4,21 +4,21 @@ from urllib import parse
 class LinkFinder(HTMLParser):
 
 	def __init__(self, base_url, page_url):
-		super().__init__();
-		self.base_url = base_url;
-		self.page_url = page_url;
-		self.links = set();
+		super().__init__()
+		self.base_url = base_url
+		self.page_url = page_url
+		self.links = set()
 
 	# When we call HTMLParser feed(), this function is calld when it encounters an openning tag <a>
 	def handle_starttag(self, tag, attrs):
 		if tag == 'a':
 			for (attribute, value) in attrs:
 				if attribute == 'href':
-					url = parse.urljoin(self.base_url, value);
-					self.links.add(url);
+					url = parse.urljoin(self.base_url, value)
+					self.links.add(url)
 
 	def page_links(self):
-		return self.links;
+		return self.links
 		
 	def error(self, messages):
-		pass;
+		pass
