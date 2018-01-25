c1bde913ee2b546165d2df3000a2552ed1fd6577
SKNIRBHAY <sknirbhay@gmail.com>
2018-01-24 21:08:02 -0800

    Update domain.py

--- domain.py
+++ domain.py
@@ -3,15 +3,15 @@ from urllib.parse import urlparse
 # Get domain name (example.com)
 def get_domain_name(url):
 	try:
-		results = get_sub_domain_names(url).split('.');
-		return results[-2] + '.' + results[-1];
+		results = get_sub_domain_names(url).split('.')
+		return results[-2] + '.' + results[-1]
 	except:
 		return ''
 
 def get_sub_domain_names(url):
 	try:
-		return urlparse(url).netloc; # Returns only the network location of a website
+		return urlparse(url).netloc # Returns only the network location of a website
 	except:
-		return '';
+		return ''
 
 		
